42d8ccb7fc87ad3a47f8306ccd947a96267ba6cc
Ursula Montag <montagu666@gmail.com>
2022-05-13 16:38:57 +0200

    Create component to render list

--- src/components/entriesList/EntriesList.jsx
+++ src/components/entriesList/EntriesList.jsx
@@ -1,13 +1,13 @@
 import Entry from '../entry/Entry';
 
-export default function EntriesList({ entries }) {
+export default function EntriesList({ entries, locations }) {
 	return (
 		<ul>
 			{entries.map(entry => {
 				const index = entries.filter(({ id }) => id === entry.id);
 				return (
 					<li key={entry.id}>
-						<Entry index={index} />
+						<Entry index={index} locations={locations} />
 					</li>
 				);
 			})}
