f976f5013fc2e527003f408fad667556eaf018e6
Ursula Montag <montagu666@gmail.com>
2022-05-14 19:25:25 +0200

    Customize props for new data structure

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
 import EntriesList from '../src/components/entriesList/EntriesList';
 import { getEntries } from '../src/services/get-entries';
-import { getLocations } from '../src/services/get-locations';
 
 export function getStaticProps() {
 	const entries = getEntries();
-	const locations = getLocations();
 	return {
 		props: {
 			entries: entries,
-			locations: locations,
 		},
 	};
 }
 
-export default function Home({ entries, locations }) {
-	return <EntriesList entries={entries} locations={locations} />;
+export default function Home({ entries }) {
+	return <EntriesList entries={entries} />;
 }
--- src/components/entriesList/EntriesList.jsx
+++ src/components/entriesList/EntriesList.jsx
@@ -1,15 +1,13 @@
 import Entry from '../entry/Entry';
 import { StyledList } from './EntriesList.styled';
-import { extractLocation } from '../../lib/helpers';
 
-export default function EntriesList({ entries, locations }) {
+export default function EntriesList({ entries }) {
 	return (
 		<StyledList>
 			{entries.map(entry => {
-				const location = extractLocation(entry, locations);
 				return (
 					<li key={entry.id}>
-						<Entry entry={entry} location={location} />
+						<Entry entry={entry} />
 					</li>
 				);
 			})}
