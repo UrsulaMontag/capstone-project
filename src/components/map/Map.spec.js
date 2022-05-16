61eaf95c62e8cfa52ccd6d976e963ad286ef494f
Ursula Montag <montagu666@gmail.com>
2022-05-16 20:58:58 +0200

    Create map-test

--- src/components/map/Map.spec.js
+++ src/components/map/Map.spec.js
@@ -1,30 +1,15 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import Map from './Map';
+import { render } from '@testing-library/react';
+import dynamic from 'next/dynamic';
+
+const MapWithNoSSR = dynamic(() => import('./Map'), {
+	ssr: false,
+});
 
 describe('Map', () => {
 	it('renders zoomable map only', () => {
-		render(
-			<Map
-				entry={{
-					id: 1,
-					date: '2022-08-05',
-					location: [48.745537999999996, 9.4452515],
-					name: 'Feuersalamander',
-					alive: true,
-					number: 3,
-					topography: 'Steinhaufen hinter Haus',
-					descripcion: 'Drei muntere Salamander am Stammplatz unter dem Steinhaufen.',
-				}}
-			/>
-		);
+		const { container } = render(<MapWithNoSSR />);
 
-		const name = screen.queryByText(/Feuersalamander/i);
-		const location1 = screen.queryByText(/48.745537999999996/i);
-		const location2 = screen.queryByText(/9.4452515/i);
-
-		expect(name).toBeInTheDocument();
-		expect(location1).toBeInTheDocument();
-		expect(location2).toBeInTheDocument();
+		expect(container.firstChild).toBeNull();
 	});
 });
