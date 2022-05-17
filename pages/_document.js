e8d8f2b7046c82da5f671839809ca9fa45d49385
Ursula Montag <montagu666@gmail.com>
2022-05-17 11:22:34 +0200

    Install regenerator-runtime

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import 'regenerator-runtime/runtime';
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
