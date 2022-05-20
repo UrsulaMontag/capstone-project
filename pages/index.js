d9c6cb95c31acb1bdc08d2ebf2d854176b44184a
Ursula Montag <montagu666@gmail.com>
2022-05-20 13:23:57 +0200

    Delete static data-files implement state from zustand in respective components

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,14 @@
 import EntriesList from '../src/components/entriesList/EntriesList';
-import { getEntries } from '../src/services/get-entries';
 import dynamic from 'next/dynamic';
 import { StyledMap } from '../src/components/ui/Map.styled';
 
-export function getStaticProps() {
-	const entries = getEntries();
-	return {
-		props: {
-			entries: entries,
-		},
-	};
-}
-
-export default function Home({ entries }) {
+export default function Home() {
 	const MapWithNoSSR = dynamic(() => import('../src/components/map/Map'), {
 		ssr: false,
 	});
 	return (
 		<>
-			<EntriesList entries={entries} />
+			<EntriesList />
 			<StyledMap>
 				<MapWithNoSSR />
 			</StyledMap>
