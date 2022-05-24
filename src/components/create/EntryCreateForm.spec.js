cc64d3fe437e86e4c450b6f534e63be09d6aa0af
Ursula Montag <montagu666@gmail.com>
2022-05-24 12:51:42 +0200

    Create test - has bug

---
+++ src/components/create/EntryCreateForm.spec.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import EntryCreateForm from '../create/EntryCreateForm';
+
+describe('Form', () => {
+	it('renders three input fields, two radiobuttons in a fieldset,', () => {
+		render(<EntryCreateForm />);
+		const name = screen.getByLabelText(/Name/i);
+		const number = screen.getByLabelText(/Anzahl Individuen/i);
+		const topography = screen.getByLabelText(/Fundort Beschreibung/i);
+		const description = screen.getByLabelText(/Beschreibe Entdeckung/i);
+
+		expect(name).toBeInTheDocument();
+		expect(number).toBeInTheDocument();
+		expect(topography).toBeInTheDocument();
+		expect(description).toBeInTheDocument();
+
+		render(<EntryCreateForm />);
+		const isAlive = screen.getByLabelText(/lebend/i);
+		const isDead = screen.getByLabelText(/tot/i);
+
+		expect(isAlive).toBeInTheDocument();
+		expect(isDead).toBeInTheDocument();
+
+		const button = screen.getByRole('button', { name: /Eintrag Erstellen/i });
+		expect(button).toBeInTheDocument();
+	});
+});
