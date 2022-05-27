95db377d9994ffb7be1f731261ed5461719204cc
Ursula Montag <montagu666@gmail.com>
2022-05-27 22:24:52 +0200

    Give index in props

--- src/components/entriesList/EntriesList.jsx
+++ src/components/entriesList/EntriesList.jsx
@@ -4,13 +4,14 @@ import { StyledList } from '../ui/ListEntries.styled';
 
 export default function EntriesList() {
 	const entries = useStore(state => state.entries);
-	console.log(entries);
 	return (
 		<StyledList>
 			{entries.map(entry => {
+				const index = entries.findIndex(entry_ => entry_.id === entry.id);
+
 				return (
 					<li key={entry.id}>
-						<Entry entry={entry} />
+						<Entry entry={entry} index={index} />
 					</li>
 				);
 			})}
