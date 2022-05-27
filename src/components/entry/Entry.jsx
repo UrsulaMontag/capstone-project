0c1eca0582594e1a17768e5be6d15974e3e721ec
Ursula Montag <montagu666@gmail.com>
2022-05-27 22:31:10 +0200

    Add delete button to entry

--- src/components/entry/Entry.jsx
+++ src/components/entry/Entry.jsx
@@ -1,7 +1,17 @@
 import { StyledEntry } from '../ui/Entry.styled';
 import Typography from '../ui/Typography';
+import useStore from '../../lib/useStore';
+import { useState } from 'react';
+import RenderIf from '../global/RenderIf';
 
-export default function Entry({ entry }) {
+export default function Entry({ entry, index }) {
+	const [isDeleteMode, setIsDeleteMode] = useState(false);
+	const deleteEntry = useStore(state => state.deleteEntry);
+
+	function toggleDeleteMode() {
+		setIsDeleteMode(!isDeleteMode);
+	}
+
 	return (
 		<StyledEntry>
 			<Typography variant="h3">Name:</Typography>
@@ -11,6 +21,37 @@ export default function Entry({ entry }) {
 				lng: {entry.location[0]} <br />
 				lat: {entry.location[1]}
 			</Typography>
+			<RenderIf isTrue={!isDeleteMode}>
+				<button
+					type="button"
+					variant="delete"
+					onClick={() => {
+						toggleDeleteMode();
+					}}
+				>
+					X
+				</button>
+			</RenderIf>
+			<RenderIf isTrue={isDeleteMode}>
+				<button
+					type="button"
+					variant="confirmation"
+					onClick={() => {
+						deleteEntry(index);
+					}}
+				>
+					Unwiederruflich Löschen
+				</button>
+				<button
+					type="button"
+					variant="exit"
+					onClick={() => {
+						toggleDeleteMode();
+					}}
+				>
+					Abbrechen
+				</button>
+			</RenderIf>
 		</StyledEntry>
 	);
 }
