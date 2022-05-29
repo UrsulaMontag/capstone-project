25e3a6d737aa5ee310cd4dfb368dc85077aab8ee
Ursula Montag <montagu666@gmail.com>
2022-05-29 21:48:07 +0200

    Adjust styliing to removed changes

--- src/components/ui/Button.styled.js
+++ src/components/ui/Button.styled.js
@@ -1,111 +1,38 @@
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-	background-color: #342293;
+	background: var(--button-bg-color);
+	color: var(--text-color-dark);
 	font-size: 1.6rem;
-	color: white;
-	padding: 5px;
-	margin-bottom: 12px;
+	padding: 0.5rem;
 	border-radius: 20px;
 	border: none;
 	width: 55%;
-	display: flex;
-	align-self: center;
-	justify-content: center;
-	${({ variant }) => variant == 'warning' && css` margin-top: 10px;}`}
+
+	box-shadow: 6px -3px var(--shadow);
+	filter: drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 0.2));
+
+	${({ variant }) =>
+		variant == 'warning' &&
+		css`
+			color: var(--color-warning-red);
+			::hover {
+				border: 3px solid var(--color-warning-red);
+			}
+		`};
 	${({ variant }) =>
 		variant === 'smallDo' &&
 		css`
-			background-color: ${props => (props.href === props.pathName ? '#ffa375' : 'white')};
-			width: 55px;
-			border-radius: 50%;
-			padding: 5px;
-			margin: 7px;
-			border: none;
-			display: flex;
-			justify-content: center;
-		`}
+			width: 4.6rem;
+			height: 4.6rem;
+			font-weight: 700;
+			border-radius: 10px;
+		`};
+
+	:hover {
+		background: var(--button-bg-color--hover);
+		border: 2px solid var(--main-bg-color);
+	}
 `;
 
 export default Button;
-
-// import styled from 'styled-components';
-
-// export default function Button({ children, variant, ...rest }) {
-// 	switch (variant) {
-// 		case 'warning':
-// 			return <StyledWarningButton {...rest}>{children}</StyledWarningButton>;
-// 		case 'smallDo':
-// 			return <StyledSmallDoButton {...rest}>{children}</StyledSmallDoButton>;
-// 		default:
-// 			return <StyledButton {...rest}>{children}</StyledButton>;
-// 	}
-// }
-
-// const StyledButton = styled.button`
-// 	width: 15rem;
-// 	height: 4.6rem;
-
-// 	background: var(--button-bg-color);
-// 	color: var(--text-color-dark);
-// 	border-radius: 20px;
-
-// 	box-shadow: 6px -3px var(--shadow);
-// 	filter: drop-shadow(0 4 0 var(--shadow);
-
-// 	box-shadow: 6px -3px var(--shadow);
-// 	filter: drop-shadow(0 4 0 var(--shadow));
-
-// 	:active {
-// 		box-shadow: none;
-// 		filter: none;
-//     }
-//     :hover {
-// 		background: var(--button-bg-color--hover);
-// 		border: 2px solid var(--main-bg-color);
-// 	}
-// `;
-
-// const StyledWarningButton = styled.button`
-// 	width: 35rem;
-// 	height: 4.6rem;
-
-// 	background: var(--button-bg-color);
-// 	color: var(--color-warning-red);
-// 	border-radius: 20px;
-
-// 	box-shadow: 6px -3px var(--shadow);
-// 	filter: drop-shadow(0 4 0 var(--shadow));
-
-// 	:hover {
-// 		background: var(--button-bg-color--hover);
-// 		border: 3px solid var(--color-warning-red);
-// 	}
-// 	:active {
-// 		box-shadow: none;
-// 		filter: none;
-// 	}
-// `;
-
-// const StyledSmallDoButton = styled.button`
-// 	width: 4.6rem;
-// 	height: 4.6rem;
-
-// 	background: var(--button-bg-color);
-// 	color: var(--color-dark);
-// 	border-radius: 10px;
-
-// 	font-weight: 700;
-
-// 	box-shadow: 6px -3px var(--shadow);
-// 	filter: drop-shadow(0 4 0 var(--shadow));
-
-// 	:active {
-// 		box-shadow: none;
-// 		filter: none;
-// 	}
-// 	:hover {
-// 		background: var(--button-bg-color--hover);
-// 		border: 2px solid var(--main-bg-color);
-// 	}
-// `;
