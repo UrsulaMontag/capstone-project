158a2f75ed8295ab740d586c406040d5766a773e
Ursula Montag <montagu666@gmail.com>
2022-05-29 15:27:13 +0200

    Import entriesList dynamic

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
-import EntriesList from '../src/components/entriesList/EntriesList';
+//import EntriesList from '../src/components/entriesList/EntriesList';
 import dynamic from 'next/dynamic';
 import { StyledMap } from '../src/components/ui/Map.styled';
 import EntryCreateForm from '../src/components/form/EntryCreateForm';
 const MapWithNoSSR = dynamic(() => import('../src/components/map/Map'), {
 	ssr: false,
 });
+const EntriesListWithNoSSR = dynamic(() => import('../src/components/entriesList/EntriesList'), {
+	ssr: false,
+});
 
 export default function Home() {
 	return (
 		<>
-			<EntriesList />
+			<EntriesListWithNoSSR />
 			<StyledMap>
 				<MapWithNoSSR />
 			</StyledMap>
