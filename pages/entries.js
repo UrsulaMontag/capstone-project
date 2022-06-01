e24cd2fbb94a0d1058d5967c900cf75098ce723f
Ursula Montag <montagu666@gmail.com>
2022-06-01 12:46:25 +0200

    Adjust data in entries page

--- pages/entries.js
+++ pages/entries.js
@@ -1,12 +1,29 @@
 import EntriesList from '../src/components/entriesList/EntriesList';
 import Box from '../src/components/ui/MainBox.styled';
 import Typography from '../src/components/ui/Typography';
+import getEntries from '../src/services/get-entries';
+import { SWRConfig } from 'swr';
+import swrFetcher from '../src/lib/db/swr-fetcher';
 
-export default function Entries() {
+export async function getStaticProps() {
+	const entries = await getEntries();
+
+	return {
+		props: {
+			fallback: {
+				'/api/entries': entries,
+			},
+		},
+	};
+}
+
+export default function Entries({ fallback }) {
 	return (
-		<Box variant="cardList">
-			<Typography variant="h2">Deine Entdeckungen</Typography>
-			<EntriesList />
-		</Box>
+		<SWRConfig value={{ fetcher: swrFetcher, fallback }}>
+			<Box variant="cardList">
+				<Typography variant="h2">Deine Entdeckungen</Typography>
+				<EntriesList />
+			</Box>
+		</SWRConfig>
 	);
 }
