ac35d8708f39306577d522b708f9e6473c7eecb5
Ursula Montag <montagu666@gmail.com>
2022-06-04 14:13:21 +0200

    Remove unnecessary import

--- pages/entries.js
+++ pages/entries.js
@@ -1,14 +1,12 @@
 import Box from '../src/components/ui/MainBox.styled';
 import Typography from '../src/components/ui/Typography';
-import dynamic from 'next/dynamic';
-const ListWithNoSSR = dynamic(() => import('../src/components/entriesList/EntriesList'), {
-	ssr: false,
-});
+import EntriesList from '../src/components/entriesList/EntriesList';
+
 export default function Entries() {
 	return (
 		<Box variant="cardList">
 			<Typography variant="h2">Deine Entdeckungen</Typography>
-			<ListWithNoSSR />
+			<EntriesList />
 		</Box>
 	);
 }
